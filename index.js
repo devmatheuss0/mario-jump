dc9585522daa362a10d45b52ab15ee4d766bde73
Matheus Silva <dev.matheuscs@gmail.com>
2023-03-05 00:14:51 -0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const loop = setInterval(() =>{
     mario.style.animation = 'none'
     mario.style.bottom = `${marioPosition}px`
 
-    mario.src = './img/Mario_Death.png'
+    mario.src = 'Mario_Death.png'
     mario.style.width = '75px'
     mario.style.marginLeft = '50px'
 
